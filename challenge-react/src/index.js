891a477bbc862fb02a5ca2282633a9fb082137ff
RakeshSagirta <rakeshsagitra9@gmail.com>
2019-11-15 15:04:06 +0530

    react app changes to js

--- challenge-react/src/index.js
+++ challenge-react/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ListScanReport from './components/ListScanReports';
 import AddScanReport from './components/AddScanReports';
