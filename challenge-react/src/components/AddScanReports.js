faab24e048d264e0dd9012d100dd635c25c61c4a
RakeshSagirta <rakeshsagitra9@gmail.com>
2019-11-15 12:35:38 +0530

    Make react hooks changes on list and details

--- challenge-react/src/components/AddScanReports.js
+++ challenge-react/src/components/AddScanReports.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import ScanReportService from '../ScanReportService';
 import TextField from '@material-ui/core/TextField';
-import Link from 'react-router-dom/Link'
+import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
 
-export default function AddScanReport() {
+export default function AddScanReport(props) {
     const [report, setReport] = useState({
         status: '',
         repositoryName: '',
@@ -18,14 +18,7 @@ export default function AddScanReport() {
         scanningAt: '',
         finishedAt: ''
     });
-    // const [status, setStatus] = useState('');
-    // const [repositoryName, setRepositoryName] = useState('');
-    // const [findings, setFindings] = useState({});
-    // const [queuedAt, setQueuedAt] = useState('');
-    // const [scanningAt, setScanningAt] = useState('');
-    // const [finishedAt, setFinishedAt] = useState('');
-    //console.log(report.findings.ruleId)
-    const addScanReport = (props) => {
+    const addScanReport = () => {
         ScanReportService.create(report)
             .then(json => {
                 if (![200, 201].includes(json.data.statusCode)) {
@@ -38,9 +31,6 @@ export default function AddScanReport() {
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        console.log(report)
-        console.log(name)
-        console.log(value)
         setReport((prevState) => {
             return ({ ...prevState, [name]: value })
         });
@@ -49,19 +39,15 @@ export default function AddScanReport() {
 
     const handleFindingsChange = (e) => {
         const { name, value } = e.target;
-        console.log(report)
-        console.log(name)
-        console.log(value)
         report.findings[name] = value;
         setReport((prevState) => {
-            console.log(prevState)
             return ({ ...prevState, findings: { ...prevState.findings, [name]: value } })
         });
     }
 
     return (
         <Paper className='root'>
-            <div className="form">
+            <div>
                 <Link className="link" to="/">
                     <Button className='primary-button'>list of Scan Results</Button>
                 </Link>
@@ -69,116 +55,116 @@ export default function AddScanReport() {
                     <h2 className="text-center">Add Scan Result</h2>
                     <div className="row justify-content-md-center">
                         <div className="col-md-6 col-md-offset-3">
-                            <form className="form" noValidate autoComplete="off">
-                                <div className="form-group">
-                                    <TextField
-                                        id="standard-basic"
-                                        label="Status"
-                                        margin="normal"
-                                        name="status"
-                                        className="form-control textfield"
-                                        onChange={handleChange}
-                                        value={report.status}
-                                    />
-                                </div>
+                            {/* <form className="form" noValidate autoComplete="off"> */}
+                            <div className="form-group">
+                                <TextField
+                                    id="standard-basic"
+                                    label="Status"
+                                    margin="normal"
+                                    name="status"
+                                    className="form-control textfield"
+                                    onChange={handleChange}
+                                    value={report.status}
+                                />
+                            </div>
 
-                                <div className="form-group">
-                                    <TextField
-                                        id="standard-basic"
-                                        label="Repository Name"
-                                        margin="normal"
-                                        name="repositoryName"
-                                        className="form-control textfield"
-                                        onChange={handleChange}
-                                        value={report.repositoryName}
-                                    />
-                                </div>
+                            <div className="form-group">
+                                <TextField
+                                    id="standard-basic"
+                                    label="Repository Name"
+                                    margin="normal"
+                                    name="repositoryName"
+                                    className="form-control textfield"
+                                    onChange={handleChange}
+                                    value={report.repositoryName}
+                                />
+                            </div>
 
-                                <div className="form-group">
-                                    <TextField
-                                        id="datetime-local"
-                                        label="Queued At"
-                                        name="queuedAt"
-                                        type="datetime-local"
-                                        value={report.queuedAt}
-                                        defaultValue={new Date()}
-                                        className="form-control textfield"
-                                        onChange={handleChange}
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
-                                </div>
+                            <div className="form-group">
+                                <TextField
+                                    id="datetime-local"
+                                    label="Queued At"
+                                    name="queuedAt"
+                                    type="datetime-local"
+                                    value={report.queuedAt}
 
-                                <div className="form-group">
-                                    <TextField
-                                        id="datetime-local"
-                                        label="Scanning At"
-                                        name="scanningAt"
-                                        type="datetime-local"
-                                        defaultValue={new Date()}
-                                        value={report.scanningAt}
-                                        className="form-control textfield"
-                                        onChange={handleChange}
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
-                                </div>
+                                    className="form-control textfield"
+                                    onChange={handleChange}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                            </div>
 
-                                <div className="form-group">
-                                    <TextField
-                                        id="datetime-local"
-                                        label="Finished At"
-                                        name="finishedAt"
-                                        type="datetime-local"
-                                        defaultValue={new Date()}
-                                        className="form-control textfield"
-                                        value={report.finishedAt}
-                                        onChange={handleChange}
-                                        InputLabelProps={{
-                                            shrink: true,
-                                        }}
-                                    />
-                                </div>
-                                Findings :
+                            <div className="form-group">
+                                <TextField
+                                    id="datetime-local"
+                                    label="Scanning At"
+                                    name="scanningAt"
+                                    type="datetime-local"
+
+                                    value={report.scanningAt}
+                                    className="form-control textfield"
+                                    onChange={handleChange}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <TextField
+                                    id="datetime-local"
+                                    label="Finished At"
+                                    name="finishedAt"
+                                    type="datetime-local"
+
+                                    className="form-control textfield"
+                                    value={report.finishedAt}
+                                    onChange={handleChange}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                            </div>
+                            Findings :
                                     <div className="form-group">
-                                    <TextField
-                                        id="standard-basic"
-                                        label="Rule Id"
-                                        margin="normal"
-                                        name="ruleId"
-                                        className="form-control textfield"
-                                        value={report.findings.ruleId || ''}
-                                        onChange={handleFindingsChange}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <TextField
-                                        id="standard-basic"
-                                        label="Description"
-                                        margin="normal"
-                                        name="description"
-                                        className="form-control textfield"
-                                        value={report.findings.description || ''}
-                                        onChange={handleFindingsChange}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <TextField
-                                        id="standard-basic"
-                                        label="Severity"
-                                        margin="normal"
-                                        name="severity"
-                                        className="form-control textfield"
-                                        onChange={handleFindingsChange}
-                                        value={report.findings.severity || ''}
-                                    />
-                                </div>
-                                <div>
-                                    <button type="button" onClick={addScanReport} className="btn btn-primary">Submit</button>
-                                </div>
-                            </form>
+                                <TextField
+                                    id="standard-basic"
+                                    label="Rule Id"
+                                    margin="normal"
+                                    name="ruleId"
+                                    className="form-control textfield"
+                                    value={report.findings.ruleId || ''}
+                                    onChange={handleFindingsChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <TextField
+                                    id="standard-basic"
+                                    label="Description"
+                                    margin="normal"
+                                    name="description"
+                                    className="form-control textfield"
+                                    value={report.findings.description || ''}
+                                    onChange={handleFindingsChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <TextField
+                                    id="standard-basic"
+                                    label="Severity"
+                                    margin="normal"
+                                    name="severity"
+                                    className="form-control textfield"
+                                    onChange={handleFindingsChange}
+                                    value={report.findings.severity || ''}
+                                />
+                            </div>
+                            <div>
+                                <button type="button" onClick={addScanReport} className="btn btn-primary">Submit</button>
+                            </div>
+                            {/* </form> */}
                         </div>
                     </div>
                 </form>
--- challenge-react/src/components/ScanReportDetails.js
+++ challenge-react/src/components/ScanReportDetails.js
@@ -1,84 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ScanReportService from '../ScanReportService';
-import axios from 'axios';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
-import Link from 'react-router-dom/Link'
+import { Link } from 'react-router-dom'
 import Moment from 'react-moment';
 import 'moment-timezone';
-let styles = {
-    marginTop: '100px'
-};
 
-class ScanReportDetails extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = { details: {} };
-        this.documentService = new ScanReportService();
-    }
-    componentDidMount() {
-        ScanReportService.getById(this.props.match.params.id)
-            .then(response => this.setState({ details: response.data ? response.data.data : {} }))
-            .catch(console.log)
-    }
-
-    render() {
-        // const classes = useStyles();
-        // const rowId = this.props.row && this.props.row.id;
-        const { details } = this.state
-        const findings = details && details.findings || {}
-        return (
-            <Paper >
-                <Link className="link" to="/">
-                    <Button className='primary-button'>back to list of scan reports</Button>
-                </Link>
-                <h3>Scan Report Details</h3>
-                <Card className='card'>
-                    <CardContent>
-                        Status : <Typography className='title' color="textSecondary" align="center" gutterBottom>
-                            {details.status}
-                        </Typography>
-                        Findings
+export default function ScanReportDetails(props) {
+    const [details, setDetails] = useState({});
+    const [detailsExist, setDetailsExist] = useState(false);
+    useEffect(() => {
+        if (!detailsExist) {
+            ScanReportService.getById(props.match.params.id)
+                .then(response => setDetails(response.data ? response.data.data : {}))
+                .catch(console.log)
+            setDetailsExist(true)
+        }
+    });
+    // const classes = useStyles();
+    // const rowId = this.props.row && this.props.row.id;
+    const findings = details && details.findings || {}
+    return (
+        <Paper >
+            <Link className="link" to="/">
+                <Button className='primary-button'>back to list of scan reports</Button>
+            </Link>
+            <h3>Scan Report Details</h3>
+            <Card className='card'>
+                <CardContent>
+                    Status : <Typography className='title' color="textSecondary" align="center" gutterBottom>
+                        {details.status}
+                    </Typography>
+                    Findings
                         <Card className='card'>
-                            <CardContent>
-                                <Typography className='title' color="textSecondary" gutterBottom>
-                                    <Typography>
-                                        Rule Id : {findings.ruleId}
-                                    </Typography>
-                                    <Typography>
-                                        Description : {findings.description}
-                                    </Typography>
-                                    <Typography>
-                                        Severity : {findings.severity}
-                                    </Typography>
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        Queued At :<Typography className='title' color="textSecondary" align="center" gutterBottom>
-                            <Moment format="DD MMM YYYY hh:mm">{details.queuedAt}</Moment>
-                        </Typography>
-                        Scanning At :<Typography className='title' color="textSecondary" align="center" gutterBottom>
-                            <Moment format="DD MMM YYYY hh:mm">{details.scanningAt}</Moment>
-                        </Typography>
-                        Finished At :<Typography className='title' color="textSecondary" align="center" gutterBottom>
-                            <Moment format="DD MMM YYYY hh:mm">{details.finishedAt}</Moment>
-                        </Typography>
-                    </CardContent>
-                </Card>
+                        <CardContent>
+                            <Typography className='title' color="textSecondary" gutterBottom>
+                                Rule Id : {findings.ruleId}
+                                <br />
+                                Description : {findings.description}
+                                <br />
+                                Severity : {findings.severity}
 
-            </Paper>
-        );
-    }
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    Queued At :<Typography className='title' color="textSecondary" align="center" gutterBottom>
+                        <Moment format="DD MMM YYYY hh:mm">{details.queuedAt}</Moment>
+                    </Typography>
+                    Scanning At :<Typography className='title' color="textSecondary" align="center" gutterBottom>
+                        <Moment format="DD MMM YYYY hh:mm">{details.scanningAt}</Moment>
+                    </Typography>
+                    Finished At :<Typography className='title' color="textSecondary" align="center" gutterBottom>
+                        <Moment format="DD MMM YYYY hh:mm">{details.finishedAt}</Moment>
+                    </Typography>
+                </CardContent>
+            </Card>
+
+        </Paper>
+    );
 }
-
-export default ScanReportDetails;
